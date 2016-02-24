2fe9fb8677ccc4333a55cb5a1350deb7f7eb1320
chplushsieh <chplushsieh@gmail.com>
2016-02-24 14:55:20 -0600

    pathsim done

--- hw1/pathsim.py
+++ hw1/pathsim.py
@@ -4,7 +4,6 @@ import pandas
 import constants
 import time
 
-
 def get_type_from_xid(xid):
     typ = ''
     if constants.tmin <= xid <= constants.tmax:
@@ -54,19 +53,76 @@ def set_relation(matrixPT, matrixPV, matrixPA):
                 print('xid:', xtype, 'has no matched type. ')
 
 
-def create_matrices():
+def create_data():
     author = read_data(constants.AUTHOR)
     term = read_data(constants.TERM)
     venue = read_data(constants.VENUE)
 
     paper = read_data(constants.PAPER)
+    return author, term, venue, paper
 
+
+def create_matrices(author, term, venue, paper):
     matrixPT = create_matrixPX(paper, term)
     matrixPV = create_matrixPX(paper, venue)
     matrixPA = create_matrixPX(paper, author)
 
     set_relation(matrixPT, matrixPV, matrixPA)
     return matrixPT, matrixPV, matrixPA
+
+
+def create_matrixAPVPA(matrixPV, matrixPA):
+    matrixAP = matrixPA.transpose()
+
+    try:
+        matrixAPV = pandas.DataFrame.from_csv(
+            constants.datapath + 'matrixAPV.csv')
+        # print('matrixAPV is loaded')
+    except IOError:
+        matrixAPV = matrixAP.dot(matrixPV)
+        matrixAPV.to_csv(constants.datapath + 'matrixAPV.csv')
+        # print('matrixAPV is saved')
+
+    matrixVPA = matrixAPV.transpose()
+
+    try:
+        matrixAPVPA = pandas.DataFrame.from_csv(
+            constants.datapath + 'matrixAPVPA.csv')
+        matrixAPVPA.columns = matrixAPVPA.index.values
+        # print('matrixAPVPA is loaded')
+    except IOError:
+        matrixAPVPA = matrixAPV.dot(matrixVPA)
+        matrixAPVPA.to_csv(constants.datapath + 'matrixAPVPA.csv')
+        # print('matrixAPVPA is saved')
+
+    return matrixAPVPA
+
+
+def create_matrixAPTPA(matrixPT, matrixPA):
+    matrixAP = matrixPA.transpose()
+
+    try:
+        matrixAPT = pandas.DataFrame.from_csv(
+            constants.datapath + 'matrixAPT.csv')
+        # print('matrixAPT is loaded')
+    except IOError:
+        matrixAPT = matrixAP.dot(matrixPT)
+        matrixAPT.to_csv(constants.datapath + 'matrixAPT.csv')
+        # print('matrixAPT is saved')
+
+    matrixTPA = matrixAPT.transpose()
+
+    try:
+        matrixAPTPA = pandas.DataFrame.from_csv(
+            constants.datapath + 'matrixAPTPA.csv')
+        matrixAPTPA.columns = matrixAPTPA.index.values
+        # print('\nmatrixAPTPA is loaded')
+    except IOError:
+        matrixAPTPA = matrixAPT.dot(matrixTPA)
+        matrixAPTPA.to_csv(constants.datapath + 'matrixAPTPA.csv')
+        # print('\nmatrixAPTPA is saved')
+
+    return matrixAPTPA
 
 
 def top_k_similar(aid, k, matrix):
@@ -76,77 +132,53 @@ def top_k_similar(aid, k, matrix):
             matrix.loc[aid][aid] + matrix.loc[other_aid][other_aid])
 
     similar.sort_values(inplace=True, ascending=False)
-    return similar[0:k]
 
-create_matrices_start = time.time()
-matrixPT, matrixPV, matrixPA = create_matrices()
-# print('matrixPT:', matrixPT)
-# print('matrixPV:', matrixPV)
-# print('matrixPA:', matrixPA)
-print('\n matrixPT, matrixPV, matrixPA done \n')
-create_matrices_end = time.time()
-print('Time spent:', create_matrices_end - create_matrices_start)
+    return similar[1:(k+1)]
 
-matrixAP = matrixPA.transpose()
 
-matrixAPV_start = time.time()
-try:
-    matrixAPV = pandas.DataFrame.from_csv(constants.datapath + 'matrixAPV.csv')
-    print('\nmatrixAPV is loaded')
-except IOError:
-    matrixAPV = matrixAP.dot(matrixPV)
-    matrixAPV.to_csv(constants.datapath + 'matrixAPV.csv')
-    print('\nmatrixAPV is saved')
-matrixAPV_end = time.time()
-print('Time spent:', matrixAPV_end - matrixAPV_start)
+def print_result(result, author):
+    for similar_aid, score in result.iteritems():
+        print(author.loc[similar_aid][1])
+        # print(similar_aid, score)
 
-matrixVPA = matrixAPV.transpose()
+preprocessing_start = time.process_time()
 
-matrixAPVPA_start = time.time()
-try:
-    matrixAPVPA = pandas.DataFrame.from_csv(
-        constants.datapath + 'matrixAPVPA.csv')
-    matrixAPVPA.columns = matrixAPVPA.index.values
-    print('\nmatrixAPVPA is loaded')
-except IOError:
-    matrixAPVPA = matrixAPV.dot(matrixVPA)
-    matrixAPVPA.to_csv(constants.datapath + 'matrixAPVPA.csv')
-    print('\nmatrixAPVPA is saved')
-matrixAPVPA_end = time.time()
-print('Time spent:', matrixAPVPA_end - matrixAPVPA_start)
+author, term, venue, paper = create_data()
+matrixPT, matrixPV, matrixPA = create_matrices(author, term, venue, paper)
+# print('\n matrixPT, matrixPV, matrixPA done \n')
+
+preprocessing_done = time.process_time()
+print('Preprocessing takes %.2f sec. ' % (
+    preprocessing_done - preprocessing_start))
+
+matrixAPVPA = create_matrixAPVPA(matrixPV, matrixPA)
+
+APVPA_done = time.process_time()
+print('\nCreating matrixAPVPA takes %.2f sec. ' % (
+    APVPA_done - preprocessing_done))
+
+matrixAPTPA = create_matrixAPTPA(matrixPT, matrixPA)
+
+APTPA_done = time.process_time()
+print('\nCreating matrixAPTPA takes %.2f sec. ' % (
+    APTPA_done - APVPA_done))
 
 print('\nThe top similar authors to Christos Faloutsos using APVPA are:\n')
-print(top_k_similar(68855, 11, matrixAPVPA))  #
+result = top_k_similar(68855, 10, matrixAPVPA)
+print_result(result, author)
+
 print('\nThe top similar authors to AnHai Doan using APVPA are:\n')
-print(top_k_similar(51360, 11, matrixAPVPA))  #
-
-matrixAPT_start = time.time()
-try:
-    matrixAPT = pandas.DataFrame.from_csv(constants.datapath + 'matrixAPT.csv')
-    print('\nmatrixAPT is loaded')
-except IOError:
-    matrixAPT = matrixAP.dot(matrixPT)
-    matrixAPT.to_csv(constants.datapath + 'matrixAPT.csv')
-    print('\nmatrixAPT is saved')
-matrixAPT_end = time.time()
-print('Time spent:', matrixAPT_end - matrixAPT_start)
-
-matrixTPA = matrixAPT.transpose()
-
-matrixAPTPA_start = time.time()
-try:
-    matrixAPTPA = pandas.DataFrame.from_csv(
-        constants.datapath + 'matrixAPTPA.csv')
-    matrixAPTPA.columns = matrixAPTPA.index.values
-    print('\nmatrixAPTPA is loaded')
-except IOError:
-    matrixAPTPA = matrixAPT.dot(matrixTPA)
-    matrixAPTPA.to_csv(constants.datapath + 'matrixAPTPA.csv')
-    print('\nmatrixAPTPA is saved')
-matrixAPTPA_end = time.time()
-print('Time spent:', matrixAPTPA_end - matrixAPTPA_start)
+result = top_k_similar(51360, 10, matrixAPVPA)
+print_result(result, author)
 
 print('\nThe top similar authors to Xifeng Yan using APTPA are:\n')
-print(top_k_similar(66631, 11, matrixAPTPA))
+result = top_k_similar(66631, 10, matrixAPTPA)
+print_result(result, author)
+
 print('\nThe top similar authors to Jamie Callan using APTPA are:\n')
-print(top_k_similar(59090, 11, matrixAPTPA))
+result = top_k_similar(59090, 10, matrixAPTPA)
+print_result(result, author)
+
+query_done = time.process_time()
+print('\nThe above queries take %.2f sec. ' % (
+    query_done - APTPA_done))
