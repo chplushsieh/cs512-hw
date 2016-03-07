b8db8eac098d0b20d764d93e805ffe96636cf72b
chplushsieh <chplushsieh@gmail.com>
2016-03-06 23:12:03 -0600

    update constatns.py

--- hw1/constants.py
+++ hw1/constants.py
@@ -8,14 +8,6 @@ ITERATION_TIME = 10
 
 ''' For pathsim.py '''
 
-'''
-I changed line 32183 from:
-32183	"Boosting'' a Positive-Data-Only Learner.
-to:
-32183	"Boosting" a Positive-Data-Only Learner.
-because python csv reader can't parse a single unpaird double quote properly.
-'''
-
 TERM = 'term'
 tmin = 1
 tmax = 13575
